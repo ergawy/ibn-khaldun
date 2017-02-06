d216a7d07fde9926c2a41107be784207253dc8af
Kareem Ergawy <kareem_ergawy@yahoo.com>
2017-02-06 18:40:16 +0100

    Clean-up all logs

--- src/cfg.c
+++ src/cfg.c
@@ -74,14 +74,11 @@ void parse_cgf_from_file(FILE *in) {
       continue;
     }
 
-    /* log("[tok: %s] ", tok); */
     BBID srcBBID = strtol(tok, NULL, 10);
-    /* log("%d: ", id); */
     PoolOffset srcBBOffset = getCFGNodeForBB(srcBBID);
     CFGNodePtr srcBB = cfgNodePool + srcBBOffset;
 
     while ((tok = strtok(NULL, " \n\t,")) != NULL) {
-      /* log("[tok: %s] ", tok); */
       BBID destBBID = strtol(tok, NULL, 10);
       PoolOffset destBBOffset = getCFGNodeForBB(destBBID);
       CFGNodePtr destBB = cfgNodePool + destBBOffset;
@@ -91,10 +88,7 @@ void parse_cgf_from_file(FILE *in) {
 
       destBB->preds[destBB->numPreds] = srcBBOffset;
       destBB->numPreds++;
-      /* log("%d, ", id); */
     }
-
-    /* log("\n"); */
   }
 
   calculateDominance();
@@ -125,25 +119,6 @@ static void calculateDominance() {
   int pot = 0;
   calculateReversePostOrder(0, &pot, rpot, visited);
 
-  /* for (int i=0 ; i<currentNumCFGNodes ; i++) { */
-  /*   CFGNodePtr n = cfgNodePool + i; */
-  /*   log("Preds of %d: ", n->id); */
-  /*   for (int j=0 ; j<n->numPreds ; j++) { */
-  /*     log("%d, ", cfgNodePool[n->preds[j]].id); */
-  /*   } */
-  /*   log("\n"); */
-
-  /*   log("Succs of %d: ", n->id); */
-  /*   for (int j=0 ; j<n->numSuccs ; j++) { */
-  /*     log("%d, ", cfgNodePool[n->succs[j]].id); */
-  /*   } */
-  /*   log("\n"); */
-  /* } */
-
-  /* for (PoolOffset i=0 ; i<currentNumCFGNodes ; i++) { */
-  /*   log("%d -> %d\n", i, cfgNodePool[rpot[i]].id); */
-  /* } */
-
   bool changed = TRUE;
 
   while(changed) {
@@ -151,17 +126,6 @@ static void calculateDominance() {
     for (int i=0 ; i<currentNumCFGNodes ; i++) {
       changed |= updateDomSet(rpot[i]);
     }
-
-
-    for (int i=1 ; i<currentNumCFGNodes ; i++) {
-      log("Dom set for BB %d:\n", i);
-      for (int j=0 ; j<cfgNodePool[i].numDoms ; j++) {
-        log("%d, ", cfgNodePool[i].doms[j]);
-      }
-      log("\n");
-    }
-
-    break;
   }
 
   free(rpot);
@@ -178,33 +142,18 @@ static bool updateDomSet(PoolOffset bbOffset) {
 
   for (int i=0 ; i<n->numPreds ; i++) {
     CFGNodePtr pred = cfgNodePool + n->preds[i];
-    log("Intersecting %d with %d\n", n->id, pred->id);
-    /* for (int j=0 ; j<n->numDoms ; j++) { */
-    /*   log("%d, ", cfgNodePool[n->doms[j]].id); */
-    /* } */
-    /* log("\n*************\n"); */
     intersectDomSets(n->doms, &(n->numDoms), pred->doms,
                      &(pred->numDoms));
-    /* for (int j=0 ; j<n->numDoms ; j++) { */
-    /*   log("%d, ", cfgNodePool[n->doms[j]].id); */
-    /* } */
-    /* log("\n*************\n"); */
   }
 
   for (int i=0 ; i<n->numDoms ; i++) {
     if (n->doms[i] == bbOffset) {
-      /* log("found element\n"); */
       return n->numDoms == oldNumDoms;
     }
   }
-  log("dom size: %d\n", n->numDoms);
+
   n->doms[n->numDoms] = bbOffset;
   n->numDoms++;
-  log("dom size: %d\n", n->numDoms);
-  for (int j=0 ; j<n->numDoms ; j++) {
-    log("%d, ", cfgNodePool[n->doms[j]].id);
-  }
-  log("\n*************\n");
   return FALSE;
 }
 
@@ -214,7 +163,6 @@ static void intersectDomSets(PoolOffset *dest, int *destSize,
   if (*destSize == 0) {
     *destSize = *srcSize;
     memcpy(dest, src, *srcSize * sizeof(PoolOffset));
-    log("new size: %d\n", *destSize);
     return;
   }
 
@@ -246,31 +194,22 @@ static void calculateReversePostOrder(PoolOffset bbOffset, int *pot,
 
   CFGNodePtr bb = cfgNodePool + bbOffset;
   visited[bbOffset] = TRUE;
-  /* log("%d: numSuccs(%d)\n", bbOffset, bb->numSuccs); */
-
   for (int i=0 ; i<bb->numSuccs ; i++) {
-    /* log("from %d to %d\n", cfgNodePool[bbOffset].id, */
-    /*     cfgNodePool[bb->succs[i]].id); */
     calculateReversePostOrder(bb->succs[i], pot, rpot, visited);
   }
 
   rpot[currentNumCFGNodes-1-*pot] = bbOffset;
-  /* log("%d -->> %d\n", (currentNumCFGNodes-1-*pot), */
-      /* bb->id); */
   ++*pot;
 }
 
 static int getCFGNodeForBB(BBID bbID) {
   for (int i=0 ; i<currentNumCFGNodes ; i++) {
     if (cfgNodePool[i].id == bbID) {
-      /* log("Found node for %d\n", bbID); */
       return i;
     }
   }
 
   if (currentNumCFGNodes == currentPoolSize) {
-    /* log("Increased pool size from %d to %d\n", currentPoolSize, */
-    /*     max(1,currentPoolSize*2)); */
     currentPoolSize = max(1, currentPoolSize*2);
     cfgNodePool = realloc(cfgNodePool,
                           currentPoolSize*sizeof(CFGNode));
@@ -279,7 +218,6 @@ static int getCFGNodeForBB(BBID bbID) {
 
   assert(currentNumCFGNodes < currentPoolSize
          && "Exceeded pool allocated size\n");
-  /* log("Created a new node for %d\n", bbID); */
   cfgNodePool[currentNumCFGNodes].id = bbID;
   cfgNodePool[currentNumCFGNodes].numSuccs = 0;
   cfgNodePool[currentNumCFGNodes].numPreds = 0;
